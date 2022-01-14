ca610f8204b78b7ffb77969eddc7e5b9cb4d3ca2
Detroit Whiskey <detroitwhiskey136@gmail.com>
2022-01-13 17:05:31 -0800

    coolbeans

--- public/content.js
+++ public/content.js
@@ -4,7 +4,7 @@ const siteData = {
     "tagline": "The best source for all covid safety information"
   },
   "quick-guide": {
-    "header": "This is a header for Quick Guide",
+    "header": "Quick Guide:",
     "content": [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc viverra nisi lacus, nec euismod lacus commodo euismod. Duis dictum ipsum sit amet aliquet eleifend. Aenean condimentum purus mi, imperdiet pulvinar lacus porttitor nec. Donec vulputate nibh urna, eu sollicitudin est viverra convallis. Ut eleifend massa nec turpis efficitur facilisis. Nunc placerat viverra nisi, at elementum est pellentesque et. Aenean augue tortor, pellentesque vitae elementum in, interdum ac eros. Pellentesque elit est, scelerisque ac tempus eu, gravida sit amet tortor. Quisque dapibus sit amet nisi in luctus. Etiam cursus imperdiet condimentum. Vestibulum luctus mauris vestibulum, commodo lacus sit amet, molestie nulla. Fusce suscipit massa nulla, sit amet suscipit leo commodo vitae. Phasellus viverra consequat elementum. Pellentesque vehicula velit quis eros pharetra maximus. Pellentesque eros velit, tempus a arcu quis, laoreet rutrum enim. Nulla erat nibh, porttitor eget pellentesque a, mattis ut lectus.",
       "Morbi et magna sit amet lorem interdum lacinia ut ut ipsum. Mauris a lectus posuere, suscipit ante ac, lacinia ligula. Sed sodales imperdiet ipsum vitae pharetra. Curabitur pellentesque aliquam sem eu venenatis. Integer velit lacus, vulputate nec dui eget, laoreet aliquet turpis. Suspendisse malesuada nunc nec lacus aliquet euismod. Mauris volutpat id nibh ut posuere. Nam ut leo diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum sed ullamcorper nisl, eget dignissim diam. Nullam lobortis tellus eget auctor cursus. Quisque diam purus, ultrices ut diam condimentum, elementum ultricies nulla.",
@@ -13,22 +13,20 @@ const siteData = {
       "Suspendisse placerat dolor nec ligula ornare tincidunt. Nam id dignissim quam. Donec dignissim sem nulla, sit amet condimentum urna rutrum ut. Donec ante lorem, finibus sed interdum vel, lobortis et sapien. Donec rutrum vitae dolor porttitor sodales. Nam eget sapien magna. Sed quis enim mollis, posuere tortor ut, feugiat ligula. Nam a finibus sapien. Vestibulum ultricies magna vel vulputate gravida. Cras faucibus suscipit malesuada. Curabitur lacinia vitae ante et ullamcorper. Mauris congue, magna sed tempor tempus, neque elit rutrum enim, sit amet aliquet nunc neque ornare eros. Proin efficitur egestas scelerisque."
     ],
     "reference": {
-      name: "Google",
-      url: "https://google.ca"
+      name: "",
+      url: ""
     }
   },
   "best-practices": {
-    "header": "This is a header for Best Practices",
+    "header": "Best Practices:",
     "content": [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc viverra nisi lacus, nec euismod lacus commodo euismod. Duis dictum ipsum sit amet aliquet eleifend. Aenean condimentum purus mi, imperdiet pulvinar lacus porttitor nec. Donec vulputate nibh urna, eu sollicitudin est viverra convallis. Ut eleifend massa nec turpis efficitur facilisis. Nunc placerat viverra nisi, at elementum est pellentesque et. Aenean augue tortor, pellentesque vitae elementum in, interdum ac eros. Pellentesque elit est, scelerisque ac tempus eu, gravida sit amet tortor. Quisque dapibus sit amet nisi in luctus. Etiam cursus imperdiet condimentum. Vestibulum luctus mauris vestibulum, commodo lacus sit amet, molestie nulla. Fusce suscipit massa nulla, sit amet suscipit leo commodo vitae. Phasellus viverra consequat elementum. Pellentesque vehicula velit quis eros pharetra maximus. Pellentesque eros velit, tempus a arcu quis, laoreet rutrum enim. Nulla erat nibh, porttitor eget pellentesque a, mattis ut lectus.",
-      "Morbi et magna sit amet lorem interdum lacinia ut ut ipsum. Mauris a lectus posuere, suscipit ante ac, lacinia ligula. Sed sodales imperdiet ipsum vitae pharetra. Curabitur pellentesque aliquam sem eu venenatis. Integer velit lacus, vulputate nec dui eget, laoreet aliquet turpis. Suspendisse malesuada nunc nec lacus aliquet euismod. Mauris volutpat id nibh ut posuere. Nam ut leo diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum sed ullamcorper nisl, eget dignissim diam. Nullam lobortis tellus eget auctor cursus. Quisque diam purus, ultrices ut diam condimentum, elementum ultricies nulla.",
-      "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris vehicula metus quis nulla tincidunt, non tincidunt mi maximus. In ut nunc molestie orci laoreet blandit eget id metus. Sed eget vulputate elit, eget ultrices neque. Aliquam sagittis posuere consectetur. Mauris a porta ligula, eu ornare massa. Integer efficitur odio arcu, vel laoreet dui sodales eu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum hendrerit iaculis ipsum, et cursus dolor laoreet ac. Vestibulum ex metus, dapibus id lacinia nec, eleifend vitae ante. Duis ac nisi pharetra, pharetra nibh id, consectetur diam. Ut ornare facilisis sem, sed semper felis tristique rutrum. Duis euismod enim id aliquet tincidunt.",
-      "Vivamus gravida mi nunc, at lacinia libero porttitor sit amet. Suspendisse vehicula ante felis, dictum tincidunt nunc malesuada a. Cras justo neque, rutrum eget auctor in, tempus at odio. Curabitur condimentum rutrum nulla, a auctor lacus tristique eu. Curabitur tempor vehicula felis sed sagittis. Aliquam imperdiet auctor odio, malesuada posuere ipsum vehicula quis. Vivamus efficitur enim sed sodales mattis.",
-      "Suspendisse placerat dolor nec ligula ornare tincidunt. Nam id dignissim quam. Donec dignissim sem nulla, sit amet condimentum urna rutrum ut. Donec ante lorem, finibus sed interdum vel, lobortis et sapien. Donec rutrum vitae dolor porttitor sodales. Nam eget sapien magna. Sed quis enim mollis, posuere tortor ut, feugiat ligula. Nam a finibus sapien. Vestibulum ultricies magna vel vulputate gravida. Cras faucibus suscipit malesuada. Curabitur lacinia vitae ante et ullamcorper. Mauris congue, magna sed tempor tempus, neque elit rutrum enim, sit amet aliquet nunc neque ornare eros. Proin efficitur egestas scelerisque."
+      "Masks -- Wear the best one you can find. We suggest N95. If you can't find one don't want to find one then find any mask. Any mask is better than no Mask. If you don't want to wear a mask then so be it. You can also decide to walk off high cliffs as well and the choice is yours.",
+      "Vaccinations -- People are always complaining about the cost of healthcare. Free shot in your arm which might save your life. Or you can go without the shot and end up in the hospital with a virus that loves to kill people who didn't pick the shot and it will cost you a lot of money. In the end you prove nothing other than you can pull yourself from the gene pool.",
+      "Social Distancing -- If you need to be around lots of people all the time then see above two points. Otherwise staying away from others for awhile until the virus is not an hourly news story is probably a good idea."
     ],
     "reference": {
-      name: "Google",
-      url: "https://google.com"
+      name: "",
+      url: ""
     }
   },
   "faq": [
@@ -37,12 +35,8 @@ const siteData = {
       "answer": "Its a virus that kills."
     },
     {
-      "question": "What is Covid?",
-      "answer": "Its a virus that kills."
-    },
-    {
-      "question": "What is Covid?",
-      "answer": "Its a virus that kills."
+      "question": "If I have Covid can i go to jakes dinner party?",
+      "answer": "We can't stop you, however, it would NOT be advised given how easy it is to pass the virus on to someone else."
     }
   ]
 }
